bc8902455516836c978b42252672c109075a0664
Kevin Zurita <kevzh05@gmail.com>
2024-12-08 21:23:03 -0500

    feat: Create migrations and views for Tasks

---
+++ todo_app/apps/tasks/views/__init__.py
@@ -0,0 +1,1 @@
+from .task_view import TaskViewSet
---
+++ todo_app/apps/tasks/views/task_view.py
@@ -0,0 +1,33 @@
+# todo_app/apps/tasks/views.py
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from ..models.task import Task
+from ..serializers.task_serializer import TaskSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
